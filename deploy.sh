e3fe777c19fa0b5ba89dbfb2805df38b709aa4be
aigledive <okayno14@gmail.com>
2024-03-10 16:19:13 +0700

    Добавлена установка шрифта Babel

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # Package dependenies
 
-sudo apt install -y i3 i3blocks compton rofi feh erlang-base
+sudo apt install -y i3 i3blocks compton rofi feh erlang-base fonts-babelstone-modern
 sudo apt install -y ./srv/packages/xkb-switch_1.6.0-0_amd64.deb
 
 # Image deploy

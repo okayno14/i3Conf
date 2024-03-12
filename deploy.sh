e566b4b13af5a136643c3514bbb7010c17cc0ef5
aigledive <okayno14@gmail.com>
2024-03-12 10:35:01 +0700

    Добавил зависимость в deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ OUT='out'
 
 # Package dependenies
 
-sudo apt install -y i3 i3blocks compton rofi feh erlang-base fonts-babelstone-modern
+sudo apt install -y i3 i3blocks compton rofi feh erlang-base fonts-babelstone-modern playerctl
 sudo apt install -y ./srv/packages/xkb-switch_1.6.0-0_amd64.deb
 
 # Image deploy

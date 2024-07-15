d55337aa012fa8bc9dd0da5dca56219b516eeeff
aigledive <okayno14@gmail.com>
2024-07-15 10:15:01 +0700

    Изменён порядок блоков, убраны лишние

--- .local/lib/i3blocks/blocklets/title.sh
+++ .local/lib/i3blocks/blocklets/title.sh
@@ -2,6 +2,7 @@
 
 i3-msg -t subscribe -m '[ "window" ]'| jq --unbuffered -Mrc '. | select(.container.focused == true).container.window_properties.title'|
 while IFS= read -r title; do
-    echo $title
+    Test=$(echo "$title" | cut -c 1-80)
+    echo "$Test"
 done
 

b7691e37c7c42c86435d36c0ef8d67ead61e9a6f
aigledive <okayno14@gmail.com>
2024-07-22 09:51:14 +0700

    Добавлен лимит вывода для блока title

--- .local/lib/i3blocks/blocklets/title.sh
+++ .local/lib/i3blocks/blocklets/title.sh
@@ -2,7 +2,7 @@
 
 i3-msg -t subscribe -m '[ "window" ]'| jq --unbuffered -Mrc '. | select(.container.focused == true).container.window_properties.title'|
 while IFS= read -r title; do
-    Test=$(echo "$title" | cut -c 1-80)
+    Test=$(echo "$title" | colrm 81)
     echo "$Test"
 done
 

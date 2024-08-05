afe1808fcb947aad1c3b110c34e84d7d9ea05458
aigledive <okayno14@gmail.com>
2024-08-05 13:50:48 +0700

    Поправил максимальную ширину для window title

--- .local/lib/i3blocks/blocklets/title.sh
+++ .local/lib/i3blocks/blocklets/title.sh
@@ -2,7 +2,7 @@
 
 i3-msg -t subscribe -m '[ "window" ]'| jq --unbuffered -Mrc '. | select(.container.focused == true).container.window_properties.title'|
 while IFS= read -r title; do
-    Test=$(echo "$title" | colrm 81)
+    Test=$(echo "$title" | colrm 75)
     echo "$Test"
 done
 
